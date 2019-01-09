e523bc2336b0544950d99ac81358b1bd75c94f18
gabriel romon <gabriel.romon@ensae.fr>
2019-01-09 21:57:26 +0100

    license

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
+/*
+    This file is part of the CSGO External Multihack MacOS
+    Copyright (C) 2016 Gabriel Romon <mariemromon@yahoo.fr> 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License version 3
+    as published by the Free Software Foundation. You may not use, modify
+    or distribute this program under any other version of the
+    GNU Affero General Public License.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  
+*/
+
+
 #include <iostream>
 #include "BSPMap.h"
 #include <stdio.h>
